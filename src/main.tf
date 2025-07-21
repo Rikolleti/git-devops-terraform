c2b5e19ab8a3ebf496cdca5d72f96ab3166dbfeb
Azhdar Mamedov <rikolleti@compute-vm-2-2-30-hdd-1751355561681.ru-central1.internal>
2025-07-21 17:44:49 +0300

    Updated

--- src/main.tf
+++ src/main.tf
@@ -8,7 +8,11 @@ terraform {
   required_version = ">=1.8.4" /*Многострочный комментарий.
  Требуемая версия terraform */
 }
-provider "docker" {}
+
+provider "docker" {
+  host     = "ssh://rikolleti@158.160.184.1"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
 
 #однострочный комментарий
 
@@ -20,19 +24,57 @@ resource "random_password" "random_string" {
   min_numeric = 1
 }
 
-/*
-resource "docker_image" {
+resource "random_password" "mysql_root_pass" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "random_password" "mysql_pass" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = true
 }
 
-resource "docker_container" "1nginx" {
+resource "docker_image" "mysql" {
+  name         = "mysql:8"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string_FAKE.resulT}"
+  name = "hello_world"
+  // name  = "example_${random_password.random_string.result}"
 
   ports {
     internal = 80
     external = 9090
   }
 }
-*/
+
+resource "docker_container" "mysql" {
+  image = docker_image.mysql.image_id
+  name = "example_${random_password.random_string.result}"
+  env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.mysql_root_pass.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.mysql_pass.result}",
+    "MYSQL_ROOT_HOST=%"
+  ]
+
+  ports {
+    ip = "127.0.0.1"
+    internal = 3306
+    external = 3306
+ }
+}
